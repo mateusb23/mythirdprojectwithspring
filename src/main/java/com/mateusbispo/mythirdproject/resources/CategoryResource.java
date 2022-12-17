0416ed9431e7dd34eee5df044eddb5e93f3061c8
mateusb23 <mateusb.bispo@gmail.com>
2022-12-17 18:00:38 -0300

    Inserting project

---
+++ src/main/java/com/mateusbispo/mythirdproject/resources/CategoryResource.java
@@ -0,0 +1,40 @@
+package com.mateusbispo.mythirdproject.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mateusbispo.mythirdproject.entities.Category;
+import com.mateusbispo.mythirdproject.repositories.CategoryRepository;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@Autowired				// ESTA ANOTAÇÃO FAZ UM PRÉ-PROCESSAMENTO POR BAIXO DOS PANOS, JÁ RESOLVENDO A DEPENDÊNCIA DE FORMA "AUTOMÁTICA" NESTA CLASSE AQUI. ELA SÓ FUNCIONOU PORQUE COLOCAMOS A ANOTAÇÃO @Component NA CLASSE CategoryRepository
+	private CategoryRepository categoryRepository;				
+	
+ /* private final CategoryRepository categoryRepository;
+	
+	public CategoryResource(CategoryRepository categoryRepository) {	// LEMBRANDO QUE, AO UTILIZARMOS A INJEÇÃO DE DEPENDÊNCIA, ESTAMOS DIZENDO QUE A
+		this.categoryRepository = categoryRepository;					// CLASSE CategoryResource DEPENDE DE UM OBJETO DO TIPO CategoryRepository 
+	} */
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> list = categoryRepository.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category cat = categoryRepository.findById(id);
+		return ResponseEntity.ok().body(cat);
+	}
+	
+}
